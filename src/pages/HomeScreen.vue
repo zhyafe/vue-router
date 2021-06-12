652eeb86c1381d355824b7208886ecfda6620376
zhyafe <1739077662@qq.com>
2021-06-12 11:37:32 +0800

    feat:nestRoute

--- src/pages/HomeScreen.vue
+++ src/pages/HomeScreen.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <p>home</p>
-    <router-link to="/user/1">动态路由</router-link>
+    <router-link to="/user/1">动态路由</router-link><br />
+    <router-link to="/nest">嵌套路由</router-link>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,32 @@
 import VueRouter from "vue-router";
 import HomeScreen from "../pages/HomeScreen";
-import UserScreen from "../pages/dynamicRoute/UserScreen";
+import DynamicScreen from "../pages/dynamicRoute/DynamicScreen";
 import NotFoundScreen from "../pages/dynamicRoute/NotFoundScreen";
+import NestScreen from "../pages/nestRoute/NestScreen";
+import NestDefaultScreen from "../pages/nestRoute/NestDefaultScreen";
+import NestTeacherScreen from "../pages/nestRoute/NestTeacherScreen";
+import NestStudentScreen from "../pages/nestRoute/NestStudentScreen";
 const routes = [
   { path: "/", component: HomeScreen },
-  { path: "/user/:id", component: UserScreen },
+  { path: "/dynamic/:id", component: DynamicScreen },
+  {
+    path: "/nest",
+    component: NestScreen,
+    children: [
+      {
+        path: "",
+        component: NestDefaultScreen,
+      },
+      {
+        path: "teacher",
+        component: NestTeacherScreen,
+      },
+      {
+        path: "student",
+        component: NestStudentScreen,
+      },
+    ],
+  },
   { path: "*", component: NotFoundScreen },
 ];
 
