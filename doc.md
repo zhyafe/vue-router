1433bf271636db71b610d9e5e74cdefcb791b4a6
张亚飞 <1739077662@qq.com>
2022-10-08 10:09:49 +0800

    feat: router init

---
+++ doc.md
@@ -0,0 +1,5 @@
+### 初始化
+
+1. 使用 createRouter, createWebHashHistory 初始化路由对象
+2. app.use(router)
+3. 在根组件中添加 <RouterView/>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+import { createRouter, createWebHashHistory } from "vue-router";
 
-createApp(App).mount('#app')
+// 2. 定义一些路由
+// 每个路由都需要映射到一个组件。
+// 我们后面再讨论嵌套路由。
+const routes = [
+  { path: "/", component: Home },
+  { path: "/about", component: About },
+];
+
+// 3. 创建路由实例并传递 `routes` 配置
+// 你可以在这里输入更多的配置，但我们在这里
+// 暂时保持简单
+const router = createRouter({
+  // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+  history: createWebHashHistory(),
+  routes, // `routes: routes` 的缩写
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
