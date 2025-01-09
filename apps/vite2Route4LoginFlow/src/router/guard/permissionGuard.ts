efedf43c17ce9c3eb5e86d200dc89d36449dce59
张亚飞 <1739077662@qq.com>
2025-01-09 14:25:39 +0800

    feat: vue路由守卫 登录流程

--- apps/vite2Route4LoginFlow/src/router/guard/permissionGuard.ts
+++ apps/vite2Route4LoginFlow/src/router/guard/permissionGuard.ts
@@ -5,20 +5,30 @@ const permissionGuard = (router: Router) => {
     console.log("🚀 ~ from:", from);
     console.log("🚀 ~ to:", to);
 
-    //白名单,无需鉴权页面
+    /**
+     * 三方登录判断 获取链接的token
+     * -存在token：执行登录；初始化（获取用户信息；添加路由；） 跳转
+     */
+
+    /**
+     * 白名单判断,无需鉴权页面
+     */
     if (["login"].includes(to.name)) {
       return true;
     }
 
-    // 登录判断
+    /**
+     * 登录判断 无token
+     */
     if (!localStorage.getItem("token")) {
       return { name: "login", replace: true };
     }
 
-    if (to.name == "user") {
-      // 返回 false 以取消导航
-      return false;
-    }
+    /**
+     * 判断是否初始化
+     *  -如果已经初始化 返回true
+     *  -如果未初始化 执行初始化逻辑：获取用户信息；获取路由信息，添加路由；
+     */
   });
 };
 
